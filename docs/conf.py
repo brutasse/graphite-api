e99d3dfdeeebe3813f071e27412e7e40cb051f83
Bruno Renié <brutasse@gmail.com>
2014-03-20 01:00:12 +0100

    Fix functions formatting

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,10 @@
 # coding: utf-8
 
 import os
+import re
 import sys
+
+from sphinx.ext import autodoc
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.pardir))
 
@@ -34,8 +37,8 @@ latex_elements = {
 }
 
 latex_documents = [
-  ('index', 'Graphite-API.tex', 'Graphite-API Documentation',
-   'Bruno Renié', 'manual'),
+    ('index', 'Graphite-API.tex', 'Graphite-API Documentation',
+     'Bruno Renié', 'manual'),
 ]
 
 man_pages = [
@@ -44,7 +47,28 @@ man_pages = [
 ]
 
 texinfo_documents = [
-  ('index', 'Graphite-API', 'Graphite-API Documentation',
-   'Bruno Renié', 'Graphite-API', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'Graphite-API', 'Graphite-API Documentation',
+     'Bruno Renié', 'Graphite-API', 'One line description of project.',
+     'Miscellaneous'),
 ]
+
+
+class RenderFunctionDocumenter(autodoc.FunctionDocumenter):
+    priority = 10
+
+    @classmethod
+    def can_document_member(cls, member, membername, isattr, parent):
+        return autodoc.FunctionDocumenter.can_document_member(
+            member, membername, isattr, parent
+        ) and parent.name == 'graphite_api.functions'
+
+    def format_args(self):
+        args = super(RenderFunctionDocumenter, self).format_args()
+        if args is not None:
+            return re.sub('requestContext, ', '', args)
+
+
+def setup(app):
+    app.add_autodocumenter(RenderFunctionDocumenter)
+
+add_module_names = False
