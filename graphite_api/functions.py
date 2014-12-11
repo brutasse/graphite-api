cff9571711b1b4c118cd3729ec37bf1a8590a7e6
Bruno Renié <brutasse@gmail.com>
2014-12-11 12:18:56 +0100

    Fix multiplySeriesWithWildcards docstring

--- graphite_api/functions.py
+++ graphite_api/functions.py
@@ -257,15 +257,12 @@ def multiplySeriesWithWildcards(requestContext, seriesList, *position):
     Example::
 
         &target=multiplySeriesWithWildcards(
-            [web.host-[0-7].avg-response.value,
-             web.host-[0-7].total-request.value], 2,3)
+            web.host-[0-7].{avg-response,total-request}.value, 2)
 
     This would be the equivalent of::
 
-        &target=multiplySeries(web.host-0.avg-response.value,
-                               web.host-0.total-request.value)
-        &target=multiplySeries(web.host-1.avg-response.value,
-                               web.host-1.total-request.value)
+        &target=multiplySeries(web.host-0.{avg-response,total-request}.value)
+        &target=multiplySeries(web.host-1.{avg-response,total-request}.value)
         ...
     """
     positions = [position] if isinstance(position, int) else position
