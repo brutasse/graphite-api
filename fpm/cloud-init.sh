c0ebee5e1ddc391c87cb8a9a6472cc76af5d7b3a
Bruno Renié <brutasse@gmail.com>
2015-10-27 16:41:25 +0100

    Also install ruby

--- fpm/cloud-init.sh
+++ fpm/cloud-init.sh
@@ -9,7 +9,7 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get -y upgrade
-apt-get -y install git ruby-dev build-essential
+apt-get -y install git ruby ruby-dev build-essential
 gem install fpm
 git clone https://github.com/brutasse/graphite-api.git
 cd graphite-api/fpm
