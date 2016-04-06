45459030245594bb52b33113c68b9afd7762bba1
Hervé BRY <herve.bry@geneanet.org>
2016-04-06 11:08:13 +0200

    Cache json/csv/raw responses if needed

--- graphite_api/app.py
+++ graphite_api/app.py
@@ -416,7 +416,10 @@ def render():
                                      ts.strftime("%Y-%m-%d %H:%M:%S"), value))
             response.seek(0)
             headers['Content-Type'] = 'text/csv'
-            return response.read(), 200, headers
+            response = (response.read(), 200, headers)
+            if use_cache:
+                app.cache.add(request_key, response, cache_timeout)
+            return response
 
         if request_options['format'] == 'json':
             series_data = []
@@ -435,7 +438,10 @@ def render():
                     series_data.append({'target': series.name,
                                         'datapoints': datapoints})
 
-            return jsonify(series_data, headers=headers)
+            response = jsonify(series_data, headers=headers)
+            if use_cache:
+                app.cache.add(request_key, response, cache_timeout)
+            return response
 
         if request_options['format'] == 'raw':
             response = StringIO()
@@ -446,7 +452,10 @@ def render():
                 response.write(u'\n')
             response.seek(0)
             headers['Content-Type'] = 'text/plain'
-            return response.read(), 200, headers
+            response = (response.read(), 200, headers)
+            if use_cache:
+                app.cache.add(request_key, response, cache_timeout)
+            return response
 
         if request_options['format'] == 'svg':
             graph_options['outputFormat'] = 'svg'
