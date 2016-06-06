4384f8b97f3f14815b2f90506008001b9341b232
John Conroy <john.conroy@interoptechnologies.com>
2016-06-06 19:54:47 -0400

    Properly close <g> tag for certain SVG images

--- graphite_api/render/glyph.py
+++ graphite_api/render/glyph.py
@@ -693,12 +693,12 @@ class Graph(object):
                     for char in re.findall(r'L -(\d+) -\d+', match.group(1)):
                         name += chr(int(char))
                     return '</g><g data-header="true" class="%s">' % name
-                svgData, subs = re.subn(r'<path.+?d="M -88 -88 (.+?)"/>',
-                                        onHeaderPath, svgData)
+                (svgData, subsMade) = re.subn(r'<path.+?d="M -88 -88 (.+?)"/>', onHeaderPath, svgData)
 
-                # Hack to replace the first </g><g> with <g>
-                svgData = svgData.replace('</g><g data-header',
-                                          '<g data-header', 1)
+                # Hack to replace the first </g><g> with <g>, and close out the last </g> at the end
+                svgData = svgData.replace('</g><g data-header', '<g data-header', 1)
+                if subsMade > 0:
+                    svgData += "</g>"
                 svgData = svgData.replace(' data-header="true"', '')
 
             fileObj.write(svgData.encode())
